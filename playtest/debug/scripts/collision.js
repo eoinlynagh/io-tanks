270c14fcdb12c2d87a41783812b945e0f2ae9d0c
Eoin <eoinlynaghgithub@gmail.com>
2019-09-13 23:12:16 -0400

    fixed debug mode, added fix to the double hit glitch
    just fixed a bunch of stuff tbqh

--- playtest/debug/scripts/collision.js
+++ playtest/debug/scripts/collision.js
@@ -1,58 +1,31 @@
-//should brickSize be global?
 //returns true if there is at least one collision point between the start and the end
+//populates the list of possible collisions
 function checkCollision(startX, startY, dx, dy, walls) {
+    allCollisions = [];
     var flag = false
     var endX = startX + dx;
     var endY = startY + dy;
     var m = dy / dx;
-    var b = endY - (m * endX)
+    var b = endY - (m * endX) //gets the equation of the line, could be changed to calculate all possible points on it faster maybe
 
-    for (let wall of walls) {
-        var potentialColl = getIntersections(wall, m, b);
-        if (!flag) {
-            for (let collision of potentialColl) {
-                if (checkCenter(startX, startY, endX, endY, collision)) {
-                    if (areEqual(collision[0], lastCollision[0], tolerance) && areEqual(collision[1], lastCollision[1], tolerance)) {
-                        break;
-                    } else {
-                        flag = true;
-                        break;
-                    }
-                }
-            };
-        }
-        if (flag) {
-            break;
-        }
-    };
-    return flag;
-}
-
-function getCollisions(startX, startY, dx, dy, walls) {
-    var allCollisions = [];
-    var endX = startX + dx;
-    var endY = startY + dy;
-    var m = dy / dx;
-    var b = endY - (m * endX)
-
-    for (let wall of walls) {
-        var potentialColl = getIntersections(wall, m, b);
+    for (let wall of walls) { //for all bricks
+        var potentialColl = getIntersections(wall, m, b); //get all intersections with the bullet line and the walls that make up the box
         var side = 0;
+
         for (let collision of potentialColl) {
-            if (checkCenter(startX, startY, endX, endY, collision)) {
-                if (areEqual(collision[0], lastCollision[0], tolerance) && areEqual(collision[1], lastCollision[1], tolerance)) {
+            if (checkCenter(startX, startY, endX, endY, collision)) { //if the intersection falls in between the start and end of the movement
+                if (areEqual(collision[0], lastCollision[0], tolerance) && areEqual(collision[1], lastCollision[1], tolerance)) { //if it did not just collide off of the same point, (tolerance == 0.00000001)
                     break;
                 } else {
-                    allCollisions.push([collision,side,wall]);
+                    flag = true;
+                    allCollisions.push([collision, side]); //sets it as a viable collision
                 }
-
-            };
+            }
             side++;
-        }
+        };
 
     };
-
-    return allCollisions;
+    return flag;
 }
 
 //if there is a collision, this moves the block to the point of intersection between the bullets path and the block, then changes the angle to reflect the collision
@@ -61,37 +34,34 @@ function getBlockBulletIntersection(startX, startY, intersectDx, intersectDy, wa
     //**1|---|
     //***|---|3
     //*****2
-    var potentialColl = getCollisions(startX, startY, intersectDx, intersectDy, walls)
+    var potentialColl = allCollisions; //possible collisions
 
     var minDistance = 90000000;
     var closest;
     var side;
-    var brick;
+    //gets the closest intersection
     for (i = 0; i < potentialColl.length; i++) {
         var cmp = getDistance(startX, startY, potentialColl[i][0]);
         if (cmp < minDistance) {
             minDistance = cmp;
             closest = potentialColl[i][0];
             side = potentialColl[i][1];
-            brick = potentialColl[i][2];
         }
     }
-    //index is the side that is bouncing off of, so with that we can change the angle
-    var endX = startX + intersectDx;
-    var endY = startY + intersectDy;
-    var m = intersectDy / intersectDx
-    var b = endY - (m * endX)
+    
     return [closest, side]
 }
 
 //gets the intersections of a block with a line (defined as slope and intersect)
 //needs to be edited to discard values that are out of bounds: change the solve for functions by giving them a lower and upper limit for the calculated value
 function getIntersections(brick, m, b) {
+    //these are the equations of the lines
     var brickLeftX = brick[0];
     var brickTopY = brick[1];
     var brickRightX = brick[0] + brickSize;
     var brickBottomY = brick[1] + brickSize;
 
+    //solve for the point of intersection with the bullet line
     var collisionIntersect1 = [solveForX(brickTopY, m, b, brickRightX, brickLeftX), brickTopY];
     var collisionIntersect2 = [brickLeftX, solveForY(m, brickLeftX, b, brickBottomY, brickTopY)];
     var collisionIntersect3 = [solveForX(brickBottomY, m, b, brickRightX, brickLeftX), brickBottomY];
@@ -102,7 +72,7 @@ function getIntersections(brick, m, b) {
 
 //returns true if the x and y coordinates are contained within the block
 function brickCollision(bulletX, bulletY, brick) {
-    var modifier = brickSize/20; //how far out the block collision extends
+    var modifier = brickSize / 20; //how far out the block collision extends
     var checkX = (bulletX >= brick[0] - modifier && bulletX <= brick[0] + brickSize + modifier);
     var checkY = (bulletY >= brick[1] - modifier && bulletY <= brick[1] + brickSize + modifier)
     return (checkX && checkY);
@@ -136,20 +106,19 @@ function lineCollisions(collX, collY, walls) {
     return false;
 }
 
-function checkCenter(startX, startY, endX, endY, collision) {
-    var condition1 = false;
-    var condition2 = false;
-    if (startX < endX) {
-        condition1 = (startX <= collision[0] && endX >= collision[0])
-    } else {
-        condition1 = (endX <= collision[0] && startX >= collision[0])
+function checkKill() {
+    if (bounceCount < bouncesToDie || frames < iFrames || DEBUG) {
+        return;
     }
-
-    if (startY < endY) {
-        condition2 = (startY <= collision[1] && endY >= collision[1])
-    } else {
-        condition2 = (startY >= collision[1] && endY <= collision[1])
+    if (getDistance(playerPosition[0], playerPosition[1], bulletCoordinates) < bulletRadius + playerRadius) {
+        alert("player 1 dead");
+        bullet = false;
+        player1Position = [canvas.width + playerRadius, canvas.height + playerRadius];
+        location.reload()
+    } else if (getDistance(player2Position[0], player2Position[1], bulletCoordinates) < bulletRadius + playerRadius) {
+        alert("player 2 dead");
+        player2Position = [canvas.width + playerRadius, canvas.height + playerRadius];
+        bullet = false;
+        location.reload()
     }
-
-    return condition1 && condition2
 }
--- playtest/debug/scripts/settings.js
+++ playtest/debug/scripts/settings.js
@@ -1,11 +1,16 @@
+DEBUG = false; //edit this to change gamemode
+
+//all settings until the if are editable
 //maze settings
 mazeSize = 100;
 mazeComplexity = 1;
 mazeDensity = 0;
+mazeDensity = 0.3 //everything speed multiplier
 removeStragglers = true;
 
 //game settings
 interval = 10
+turns = 2;
 
 //brick options
 brickColour = "lightblue";
@@ -15,25 +20,63 @@ shiftDownModifier = 1.5;
 playerColor = "#0095DD";
 player2Color = "red";
 maxCount = 3; //number of moves
-moveMultiplier = 75; //size of the circle that the player moves in
+moveMultiplier = 20; //size of the circle that the player moves in
+iFrames = 15;
 
 //bullet options
 bulletColor = "#0095DD"
 bullet2Color = "red"
 maxBounceCount = 5;
+speedMultiplier = 1
+bouncesToDie = 1;
 
 //line options
 lineColour = "white"
 disableLineBounce = true;
 
-//do not change these generally
-alreadyHit = false;
-angle = 666;
-bullet = false;
-bounceCount = 0;
-lastBounce = -666;
-turns = 2;
-count = 0;
-turn = true;
-lStopped = true;
-lastCollision = [-666, -666];
+if (!DEBUG) {
+    //maze settings
+    mazeSize = 100;
+    mazeComplexity = 1;
+    mazeDensity = 0;
+    mazeDensity = 0.3 //everything speed multiplier
+    removeStragglers = true;
+
+    //game settings
+    interval = 10
+    turns = 0;
+
+    //brick options
+    brickColour = "lightblue";
+    shiftDownModifier = 1.5;
+
+    //players options
+    playerColor = "#0095DD";
+    player2Color = "red";
+    maxCount = 3; //number of moves
+    moveMultiplier = 20; //size of the circle that the player moves in
+    iFrames = 15;
+
+    //bullet options
+    bulletColor = "#0095DD"
+    bullet2Color = "red"
+    maxBounceCount = 5;
+    speedMultiplier = 1
+    bouncesToDie = 1;
+
+    //line options
+    lineColour = "white"
+    disableLineBounce = true;
+}
+
+//these don't get touched >:(
+angle = 666; //current bullet angle
+lineAngle = angle; //current line angle
+bullet = false; //is there a bullet on screen
+bounceCount = 0; //number of bounces a bullet has when it is shot
+count = 0; //number of movements
+lStopped = true; //is the line !active
+lastCollision = [-666, -666]; //last point you collided with
+frames = 0; //frames since start of game
+tolerance = 0.00000001; //TOLERANCE for collisions
+allCollisions = [];
